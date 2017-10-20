d491e9b56aa50f8a32965841d526d0169a81ec12
Mateo Barría <mateobarria@gmail.com>
2017-10-20 15:14:58 -0300

    Update launcher.py

--- launcher.py
+++ launcher.py
@@ -1,4 +1,46 @@
 import bot
 import os
+import requests
 
-os.system('python3 bot.py'
+repositorio = 'https://raw.githubusercontent.com/mateoinc/LCBoTQ/master/'
+textos = repositorio + 'textos/'
+directorio = os.getcwd()
+
+verActual = open(directorio + '/changelog.txt','r')
+verVirtual = requests.get(repositorio + 'changelog.txt')
+
+if verActual != verVirtual:
+    url = textos + 'ayuda.txt'
+    archivo = requests.get(url)
+    destino = directorio + 'textos/ayuda.txt'
+    with open(destino,'w') as file:
+        file.write(archivo.content)
+
+    url = textos + 'frases.txt'
+    archivo = requests.get(url)
+    destino = directorio + 'textos/frases.txt'
+    with open(destino,'w') as file:
+        file.write(archivo.content)
+
+    url = textos + 'lugares.txt'
+    archivo = requests.get(url)
+    destino = directorio + 'textos/lugares.txt'
+    with open(destino,'w') as file:
+        file.write(archivo.content)
+
+    url = textos + 'puteadas.txt'
+    archivo = requests.get(url)
+    destino = directorio + 'textos/puteadas.txt'
+    with open(destino,'w') as file:
+        file.write(archivo.content)
+
+    url = repositorio + 'changelog.txt'
+    archivo = requests.get(url)
+    destino = directorio + 'changelog.txt'
+    with open(destino,'w') as file:
+       file.write(archivo.content)
+
+    os.system('python3 bot.py')
+
+elif verActual == verVirtual:
+    os.system('python3 bot.py')
