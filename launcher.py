7ece77ee03c4cab9703e45b252205ece563aab1b
Mateo Barría <mateobarria@gmail.com>
2017-10-20 21:37:24 -0300

    Deje de importar bot

--- launcher.py
+++ launcher.py
@@ -1,52 +1,57 @@
-import bot
 import os
 import requests
 
 repositorio = 'https://raw.githubusercontent.com/mateoinc/LCBoTQ/master/'
 textos = repositorio + 'textos/'
-directorio = os.getcwd()
+directorio = os.getcwd() + '/'
+verActual = open(directorio + 'version.txt','r').read()
+verVirtual = requests.get(repositorio + 'version.txt').content
 
-verActual = open(directorio + '/changelog.txt','r')
-verVirtual = requests.get(repositorio + 'changelog.txt')
-
-if verActual != verVirtual:
+if verActual !=  verVirtual:
     url = textos + 'ayuda.txt'
     archivo = requests.get(url)
     destino = directorio + 'textos/ayuda.txt'
     with open(destino,'w') as file:
-        file.write(archivo.content)
+        file.write(archivo.text)
 
     url = textos + 'frases.txt'
     archivo = requests.get(url)
     destino = directorio + 'textos/frases.txt'
     with open(destino,'w') as file:
-        file.write(archivo.content)
+        file.write(archivo.text)
 
     url = textos + 'lugares.txt'
     archivo = requests.get(url)
     destino = directorio + 'textos/lugares.txt'
     with open(destino,'w') as file:
-        file.write(archivo.content)
+        file.write(archivo.text)
 
     url = textos + 'puteadas.txt'
     archivo = requests.get(url)
     destino = directorio + 'textos/puteadas.txt'
     with open(destino,'w') as file:
-        file.write(archivo.content)
+        file.write(archivo.text)
 
     url = repositorio + 'changelog.txt'
     archivo = requests.get(url)
     destino = directorio + 'changelog.txt'
     with open(destino,'w') as file:
-       file.write(archivo.content)
+       file.write(archivo.text)
 
     url = repositorio + 'version.txt'
     archivo = requests.get(url)
     destino = directorio + 'version.txt'
     with open(destino,'w') as file:
-       file.write(archivo.content)
-    
+       file.write(archivo.text)
+       
+    url = repositorio + 'bot.py'
+    archivo = requests.get(url)
+    destino = directorio + 'bot.py'
+    with open(destino,'w') as file:
+       file.write(archivo.text)
+              
     os.system('python3 bot.py')
 
 elif verActual == verVirtual:
     os.system('python3 bot.py')
+
